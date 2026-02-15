cb8a8ad7a1885a436d184cba12cf902d418e772d
Miheer Patel <miheer2109@gmail.com>
2026-02-16 03:51:47 +0530

    feat: add cloudflared tunnel script and update server listen log

--- server/server.ts
+++ server/server.ts
@@ -24,6 +24,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
